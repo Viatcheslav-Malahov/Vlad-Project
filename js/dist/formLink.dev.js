f16a4f801f8560fa7d770e6cf16740e2c1dbd1af
slava <viatcheslavmalahov@gmail.com>
2020-12-03 18:36:22 +0300

    form

--- js/dist/formLink.dev.js
+++ js/dist/formLink.dev.js
@@ -6,12 +6,12 @@ document.querySelector('.form__link').addEventListener('click', function (event)
   if (emailInp.value == '') {
     alert('Enter email first plz');
     return false;
-  } else {
-    var xhr = new XMLHttpRequest();
-    var oData = new FormData();
-    oData.append('UserEmail', emailInp.value);
-    xhr.open('POST', 'http://95.213.249.252/vlad_form_parser', false);
-    xhr.send(oData);
-    xhr.response; //window.location.href = './form.html'
-  }
+  } // else {
+  //     let xhr = new XMLHttpRequest();
+  //     let oData = new FormData()
+  //     oData.append('UserEmail' , emailInp.value)
+  //     xhr.open('POST','http://95.213.249.252/vlad_form_parser',false)
+  //     xhr.send(oData)
+  // }
+
 });
--- js/formLink.js
+++ js/formLink.js
@@ -4,13 +4,11 @@ document.querySelector('.form__link').addEventListener('click',(event)=>{
         alert ('Enter email first plz')
         return false
     }
-    else {
-        let xhr = new XMLHttpRequest();
-        let oData = new FormData()
-        oData.append('UserEmail' , emailInp.value)
-        xhr.open('POST','http://95.213.249.252/vlad_form_parser',false)
-        xhr.send(oData)
-        xhr.response
-        //window.location.href = './form.html'
-    }
+    // else {
+    //     let xhr = new XMLHttpRequest();
+    //     let oData = new FormData()
+    //     oData.append('UserEmail' , emailInp.value)
+    //     xhr.open('POST','http://95.213.249.252/vlad_form_parser',false)
+    //     xhr.send(oData)
+    // }
 })
