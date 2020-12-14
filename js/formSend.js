5009da620ec18b919bfbbbc0fa87807cad9e7bec
slava <viatcheslavmalahov@gmail.com>
2020-12-14 16:24:51 +0300

    sendFOrm

--- js/formSend.js
+++ js/formSend.js
@@ -1,55 +1,56 @@
 let formInfo = {}
-let inputValues = document.querySelectorAll('.form_elem')
-let Agreemnet = document.querySelector('#Agreement')
-document.querySelector('.form__item_btn').addEventListener('click' ,(event)=>{
+
+let inputTextValues = document.querySelectorAll('.mainFormText')
+let inputCheckValues = document.querySelectorAll('.mainFormCheck')
+
+let Agreemnet = document.querySelector('#agreementCheck')
+
+document.querySelector('.sendFormBtn').addEventListener('click', (event) => {
     event.preventDefault()
-    
-    formInfo['Email'] = document.querySelector('#email__inp').value
-    inputValues.forEach(element => {
-        if (element == Agreemnet){
-            if(element.checked == false){
-                document.querySelector('#Agreement').classList.add('RedOutline')
-                document.querySelector('.RedAllert').classList.remove('hidden')
-                return false
-            }
-            else{
-                document.querySelector('#Agreement').classList.remove('RedOutline')
-            }
-        }
-        else {
-            if (element.value == ''){
-                element.classList.add('redBorder')
-                document.querySelector('.RedAllert').classList.remove('hidden')
-                return false
-            }
-            
-            else{
-                formInfo[element.name] = element.value
-                element.classList.remove('redBorder')
-                
-            }
-        }
-    });
-    function isNotEmpty(a){
-        return a.value != '';
+    if (Agreemnet.checked === false) {
+        alert('plz sign the agreement')
+        return false
     }
-    let y = 0 
-    for (let x =0 ; x < inputValues.length ; x++){
-        if( isNotEmpty(inputValues[x]) == false || Agreemnet.checked == false){
+
+    let y=0
+
+    for (let x = 0; x < inputTextValues.length; x++) {
+        if (isNotEmpty(inputTextValues[x]) === false) {
             y++
         }
     }
-    if (y == 0){
-        let xhr = new XMLHttpRequest();
-        let oData = new FormData()
-        oData.append('form' , JSON.stringify(formInfo))
-        xhr.open('POST','http://127.0.0.1:5500/',false)
-        xhr.send(oData)
-        //xhr.onload = function
+
+    if (y != 0) {
+        alert('fill in the fields ')
+        return false
+    }
+    else {
+        inputTextValues.forEach(element => {
+            formInfo[element.name] = element.value
+        })    
     }
 
+
+
+    inputCheckValues.forEach(element => {
+        if (element.checked === true)
+            formInfo[element.name] = element.value
+    })
+
+
+    function isNotEmpty(a) {
+        return a.value != '';
+    }
+    // for (let x =0 ; x < inputValues.length ; x++){
+    //     if( isNotEmpty(inputValues[x]) == false || Agreemnet.checked == false){
+    //         y++
+    //     }
+    // }
+    let xhr = new XMLHttpRequest();
+    let oData = new FormData()
+    oData.append('form', JSON.stringify(formInfo))
+    xhr.open('POST', 'http://95.213.249.252/db', false)
+    xhr.send(oData)
+
 })
 
-document.querySelector('.RedAllert').querySelector('i').addEventListener('click',()=>{
-    document.querySelector('.RedAllert').classList.add('hidden')
-})
