2893abff7160e96f1712a69b5cf97a25147b3a40
Slava <viatcheslavmalahov@gmail.com>
2021-04-21 02:54:54 +0300

    redisigne

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,8 @@
 let NavBar = document.querySelector('.nav__bar')
 let NavBarItem = NavBar.querySelector('.fas')
 
-NavBar.addEventListener('click',()=>{
-    if (NavBarItem.classList.contains('fa-bars')){
+NavBar.addEventListener('click', () => {
+    if (NavBarItem.classList.contains('fa-bars')) {
         NavBarItem.classList.add('fa-times')
         NavBarItem.classList.remove('fa-bars')
         document.querySelector('.nav_links_bar').classList.remove('hidden')
@@ -22,18 +22,30 @@ NavBar.addEventListener('click',()=>{
     }
 })
 
+window.onresize = function (event) {
+    if (screen.availWidth > 960) {
+        NavBarItem.classList.add('fa-bars')
+        NavBarItem.classList.remove('fa-times')
+        document.querySelector('.nav_links_bar').classList.add('hidden')
+        document.querySelector('header').classList.remove('passive_sec')
+        document.querySelector('main').classList.remove('passive_sec')
+        document.querySelector('footer').classList.remove('passive_sec')
+        document.querySelector('section').classList.remove('passive_sec')
+    }
+};
+
 let nav = document.querySelector('nav')
 
 let sticky = nav.offsetTop
 
 window.onscroll = () => {
-    if (window.pageYOffset >= sticky){
+    if (window.pageYOffset >= sticky) {
         nav.classList.add('sticky')
     }
-    else{
+    else {
         nav.classList.remove('sticky')
     }
-    if (window.pageYOffset <= sticky){
-        nav.classList.remove('sticky') 
+    if (window.pageYOffset <= sticky) {
+        nav.classList.remove('sticky')
     }
 }
